e4c69b49c4f9904e181e1914483d2af494b8da11
cameron2134 <cameron2134@users.noreply.github.com>
2019-09-15 14:12:06 +0100

    Added Reader and Parser tests.

---
+++ Timezone.Tests/ParserTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timezone.Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void DisplayTime_NullTimeParameter_NoNullExceptionThrown()
+        {
+            IParser parser = new Parser();
+
+            try
+            {
+                Assert.ThrowsException<NullReferenceException>(() => parser.DisplayTime(null, "London"));
+            }
+
+            catch (AssertFailedException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("No exception thrown"));
+            }
+        }
+
+        [TestMethod]
+        public void DisplayTime_NullTimezoneParameter_NoNullExceptionThrown()
+        {
+            IParser parser = new Parser();
+
+            try
+            {
+                Assert.ThrowsException<NullReferenceException>(() => parser.DisplayTime("09:00", null));
+            }
+
+            catch (AssertFailedException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("No exception thrown"));
+            }
+        }
+
+        [TestMethod]
+        public void DisplayTime_AllParametersNull_NoNullExceptionThrown()
+        {
+            IParser parser = new Parser();
+
+            try
+            {
+                Assert.ThrowsException<NullReferenceException>(() => parser.DisplayTime(null, null));
+            }
+
+            catch (AssertFailedException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("No exception thrown"));
+            }
+        }
+    }
+}
---
+++ Timezone.Tests/ReaderTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timezone.Tests
+{
+    [TestClass]
+    public class ReaderTests
+    {
+        [TestMethod]
+        public void Read_ValidData_ReturnsTuplesInList()
+        {
+            string lineData = $"10:00 London{Environment.NewLine}";
+            lineData += "18:00 Arizona";
+
+            IReader<string> reader = new Reader();
+            IList<Tuple<string, string>> timezoneList = reader.Read(lineData);
+
+            Assert.IsTrue(timezoneList.Count > 0);
+        }
+
+        [TestMethod]
+        public void Read_InvalidData_ReturnsEmptyList()
+        {
+            string lineData = $"Invalid text line";
+
+            IReader<string> reader = new Reader();
+            IList<Tuple<string, string>> timezoneList = reader.Read(lineData);
+
+            Assert.IsTrue(timezoneList.Count == 0);
+        }
+
+        [TestMethod]
+        public void Read_NullInputFile_ReturnsEmptyList()
+        {
+            string lineData = null;
+
+            IReader<string> reader = new Reader();
+            IList<Tuple<string, string>> timezoneList = reader.Read(lineData);
+
+            Assert.IsTrue(timezoneList.Count == 0);
+        }
+    }
+}
