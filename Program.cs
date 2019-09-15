148ced81065ec79deb012f77fd29c7a164d06cf6
cameron2134 <cameron2134@users.noreply.github.com>
2019-09-15 11:33:16 +0100

    Removed using statement as Reader no longer implements IDisposable.

--- Program.cs
+++ Program.cs
@@ -12,14 +12,12 @@ namespace Timezone
         static void Main(string[] args)
         {
             Parser timeZoneParser = new Parser();
-            using (Reader fileReader = new Reader())
-            {
-                List<Tuple<string, string>> lTimes = fileReader.Read();
+            Reader fileReader = new Reader();
+            List<Tuple<string, string>> lTimes = fileReader.Read();
 
-                foreach (Tuple<string, string> timeZone in lTimes)
-                {
-                    timeZoneParser.DisplayTime(timeZone.Item1, timeZone.Item2);
-                }
+            foreach (Tuple<string, string> timeZone in lTimes)
+            {
+                timeZoneParser.DisplayTime(timeZone.Item1, timeZone.Item2);
             }
 
             Console.WriteLine($"{Environment.NewLine}Press any key to exit.");
